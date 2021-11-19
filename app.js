004523fc697b549bda702e33a914b1f33c34d4eb
手塚真斗 <tez.0731.mst@gmail.com>
2021-11-20 00:54:57 +0900

    SQLの設置・新規登録機能

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-// const mysql = require('mysql');
+const mysql = require('mysql');
 const app = express();
 
 app.use('/public',express.static('public'));
 app.set('views','./views');
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection(
+  {host:'localhost',
+  user:'me',
+  password:'tezukamasato1370',
+  database:'users'
+  });
 
 // https://expressjs.com/ja/4x/api.html#express.urlencoded
 
@@ -15,9 +22,12 @@ app.use('/login',require('./routes/login'));
 app.use('/newregister',require('./routes/register'));
 app.use('/list-top',require('./routes/listtop'));
 
-// app.get('/login', (req,res,next) => {
+// app.post('/newregister', (req,res,next) => {
 //   res.render('login.ejs');
 // })
 
 
+module.exports = connection;
+
 app.listen(8000);
+
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session');
+// const connection = require('../app.js');
+// router.use('/',connection);
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(
+  {host:'localhost',
+  user:'root',
+  password:'tezukamasato1370',
+  database:'my_db'
+  });
 
 router.get('/', (req,res,next) => {
   res.render('register.ejs');
+  next()
+});
+
+
+router.post('/', (req,res,next) => {
+  const firstName =  req.body.first;
+  const lastName = req.body.last;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(firstName);
+  console.log(lastName);
+  //空入力の判定
+  console.log("データ受け取り完了");
+  connection.query(
+    'INSERT INTO users (firstName, lastName, email, password) VALUES (?,?,?,?)',
+    [firstName, lastName, email, password],
+    (err,results) => {
+      if(err)  throw err;
+      console.log(results);
+      req.session.id = results.insertId;
+      req.session.email = results.email;
+      req.session.password = results.password;
+    }
+  );
+  res.render('listpage.ejs');
 })
 
 module.exports = router;
