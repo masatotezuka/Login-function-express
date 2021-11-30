71e1c169f2f00062455bded3789545968bd4e279
手塚真斗 <tez.0731.mst@gmail.com>
2021-11-30 17:36:58 +0900

    微修正

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res,next) => {
-  res.render('top.ejs');
+router.get("/", (req, res, next) => {
+  res.render("top.ejs");
 });
 
 module.exports = router;
